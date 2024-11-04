61f6aaf2883c0aff0f28c71a4f9b667d0a51b930
deflillusion <defl.illusion@gmail.com>
2024-11-04 22:26:41 +0500

    Добавлен lesson-3 из back-end-101

---
+++ back-end-101/lesson-3/my_app/main.py
@@ -0,0 +1,3 @@
+# my_app/main.py
+
+from utils.helpers import sort_clothes, put_clothes, detergent, washing_programm, run_programm, washing_clothes
---
+++ back-end-101/lesson-3/my_app/utils/helpers.py
@@ -0,0 +1,44 @@
+# utils/helpers.py
+
+import time
+
+# Задача Постирать одежду
+def sort_clothes():  # Отсортировать одежду по цветам
+    print("Отсортировать одежду по цветам")
+    time.sleep(1)
+
+
+
+def put_clothes():  # Положить оперделенное количество одежды в стиральную машину
+    print("Положить оперделенное количество одежды в стиральную машину")
+    time.sleep(1)
+
+
+
+def detergent():    # Залить моющее средство в стиральную машину
+    print("Залить моющее средство в стиральную машину")
+    time.sleep(1)
+
+
+
+def washing_programm():  # Выбрать нужную программу стирки
+    print("Выбрать нужную программу стирки")
+    time.sleep(1)
+
+
+
+def run_programm():  # Запустить стиральную машину
+    print("Запустить стиральную машину")
+    time.sleep(1)
+
+
+def washing_clothes(): #Стирка одежды
+    print("Начинаем стирку")
+    time.sleep(1)
+    sort_clothes()
+    put_clothes()
+    detergent()
+    washing_programm()
+    run_programm()
+    print("Ждем пока постирается")
+washing_clothes()
