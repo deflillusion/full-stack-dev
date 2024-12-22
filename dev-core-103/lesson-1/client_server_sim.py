401c67df302c40190c6b89012455fea5c726b2d8
deflillusion <defl.illusion@gmail.com>
2024-12-22 21:44:20 +0500

    dev-core-103-1

---
+++ dev-core-103/lesson-1/client_server_sim.py
@@ -0,0 +1,16 @@
+def client_request():
+    print("Клиент отправляет запрос на сервер.")
+
+
+def server_response():
+    print("Сервер обрабатывает запрос.")
+    print("Сервер отправляет ответ: Добро пожаловать в интернет!")
+
+
+def main():
+    client_request()
+    server_response()
+
+
+if __name__ == "__main__":
+    main()
